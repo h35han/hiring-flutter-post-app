71ac0e7a050676aa741d37bfea41b50a8ee5b538
Heshan Wijesinghe <heshanto@gmail.com>
2026-08-27 04:39:12 +0530

    google fonts

--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'tokens/checkbox_theme.dart';
 import 'tokens/colors.dart';
 import 'tokens/input_theme.dart';
 
 final appThemeData = ThemeData(
+  textTheme: GoogleFonts.lexendDecaTextTheme(),
   colorScheme: ColorScheme(
     brightness: Brightness.light,
     primary: AppColors.primary1,
